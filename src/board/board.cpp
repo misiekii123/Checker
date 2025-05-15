4b3b1eab7872977c684674bc1c3855618af1eeb5
misiekii123 <92684568+misiekii123@users.noreply.github.com>
2025-05-15 23:25:04 +0200

    drawing board function prototype

--- src/board/board.cpp
+++ src/board/board.cpp
@@ -1,7 +1,15 @@
 #include "board.h"
 
 void Board::drawBoard() {
-
+    for (int i = 0; i < size.x; i++) {
+        for (int j = 0; j < size.y; j++) {
+            if ((i + j) % 2 == 0) {
+                DrawRectangle(i * 100, j * 100, 100, 100, WHITE);
+            } else {
+                DrawRectangle(i * 100, j * 100, 100, 100, BLACK);
+            }
+        }
+    }
 }
 
 Board::Board(Vector2 size) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
-#include <raylib.h>
+#include "raygui.h"
+#include "raylib.h"
 #include <iostream>
+#include "board/board.h"
 
 int main() {
     // Initialize the window
     const int screenWidth = 800;
     const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "Checker");
+
+    Board board(Vector2{8, 8});
 
     SetTargetFPS(60);
 
