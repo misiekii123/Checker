01ba4da64dd1ad20720b9542704321fcf1b6d8b8
misiekii123 <92684568+misiekii123@users.noreply.github.com>
2025-05-16 18:30:53 +0200

    fix indentation in classes

--- src/board/board.h
+++ src/board/board.h
@@ -1,10 +1,10 @@
 #include <raylib.h>
 
 class Board{
-    private:
-        Vector2 size;
-    public:
-        void drawBoard();
-        Board(Vector2 size);
+private:
+    Vector2 size;
+public:
+    void drawBoard();
+    Board(Vector2 size);
 
 };
