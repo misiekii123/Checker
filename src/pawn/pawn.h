89f132f9670e98f5bbf137a7dfad5b10ec078b7b
mkowal <mkowal.3009@gmail.com>
2025-05-16 20:18:31 +0200

    Colors added

--- src/pawn/pawn.h
+++ src/pawn/pawn.h
@@ -1,8 +1,15 @@
 #include <raylib.h>
 
+enum class Color {
+    White,
+    Black
+};
+
 class Pawn {
 private:
     Vector2 position;
+    Color pawnColor;
+    bool IsAlive;
 public:
     Vector2 getPosition();
     void changePosition(Vector2 position);
