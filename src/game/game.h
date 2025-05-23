dfc7407442c504c4034e67c2a74ef84061b5595b
mkowal <mkowal.3009@gmail.com>
2025-05-23 20:34:03 +0200

    Corrected mistake in game.h

--- src/game/game.h
+++ src/game/game.h
@@ -29,6 +29,6 @@ public:
     Game();
     std::vector<Vector2> legalMoves(Pawn* pawn);
     std::vector<Vector2> whereIsBeatingAvailable(Pawn* pawn);
-    void Game::simulateMultiBeating(Pawn* pawn, std::vector<Vector2> current_path, std::vector<Vector2>& best_path);
+    void simulateMultiBeating(Pawn* pawn, std::vector<Vector2> current_path, std::vector<Vector2>& best_path);
     std::vector<Vector2> multipleBeatings(Pawn* pawn);
 };
